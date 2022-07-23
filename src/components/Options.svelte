c1c2daa5d5b76dac855db329e02d0054ee30bb9c
Sotiris Morfakidis <sotirismorfakidis@gmail.com>
2022-07-24 02:59:09 +0300

    scrolling functionality added

--- src/components/Options.svelte
+++ src/components/Options.svelte
@@ -5,6 +5,10 @@
 	let tabs: chrome.tabs.Tab[] = [];
 
 	let searchQueryInput: any;
+	let resultsElement: any;
+
+	let firstItemIndex: number = 0;
+	let resultsShown: number = 11;
 	let selectedIndex: number = 0;
 
 	onMount(() => {
@@ -14,19 +18,42 @@
 	});
 
 	function onKeyDown(e: KeyboardEvent) {
-		console.log(e);
 		if (e.code === "Enter") {
+			e.preventDefault();
 			chrome.tabs.update(tabs[selectedIndex].id, { active: true });
-		} else if (e.code === "Tab") {
-			if (e.shiftKey && selectedIndex > 0) selectedIndex -= 1;
-			else if (!e.shiftKey && selectedIndex < 8) selectedIndex += 1;
+
+			// Moving up
+		} else if (e.code === "Tab" && e.shiftKey && selectedIndex > 0) {
+			e.preventDefault();
+			selectedIndex -= 1;
+
+			// Check if scrolling up is needed
+			if (firstItemIndex == selectedIndex) {
+				firstItemIndex -= 1;
+				resultsElement.children[selectedIndex].scrollIntoView();
+			}
+
+			// Moving down
+		} else if (e.code === "Tab" && !e.shiftKey && selectedIndex < tabs.length - 1) {
+			e.preventDefault();
+			selectedIndex += 1;
+
+			// Check if scrolling down is needed
+			if (firstItemIndex + resultsShown == selectedIndex) {
+				firstItemIndex += 1;
+				resultsElement.children[selectedIndex].scrollIntoView({
+					block: "end",
+				});
+			}
 		}
 	}
 
 	function onInputChange() {
 		chrome.runtime.sendMessage(
 			{ type: "query", body: searchQuery },
+			//{ type: "query", body: "" },
 			(response) => {
+				selectedIndex = 0;
 				tabs = response;
 			}
 		);
@@ -42,42 +69,65 @@
 		placeholder="Search for title"
 		type="text"
 	/>
-	<div class="results">
-		{#each tabs as tab, i}
-			<p class="{ i === selectedIndex ? 'selected' : ''}">{tab.title}</p>
-		{/each}
+	<div class="results-container">
+		<div class="results" bind:this={resultsElement}>
+			{#each tabs as tab, i}
+				<p class={i === selectedIndex ? "selected" : ""}>
+					{i}. {tab.title}
+				</p>
+			{/each}
+		</div>
 	</div>
 </div>
 
 <style>
+	:global(body) {
+		margin: 0;
+	}
+
 	.container {
-		width: 400px;
+		padding: 8px;
+		background-color: #282828;
+		color: #fbf1c7;
+		width: 600px;
 		display: flex;
 		flex-direction: column;
-		overflow-y: hidden;
+		/*overflow-y: hidden;*/
 		overflow-x: hidden;
 		font-family: monospace;
 		font-size: 1rem;
 	}
 
-	.results {
+	.results-container {
 		border: solid;
-		border-color: black;
-		height: 300px;
+		border-color: #b16286;
+	}
+
+	.results {
+		overflow-y: scroll;
+		height: 308px;
+	}
+
+	.results::-webkit-scrollbar {
+		display: none;
 	}
 
 	input {
-		font-family: monospace;
-		border: solid;
-		border-color: black;
+		color: #fdf1c7;
 		outline: none;
+		border: solid;
+		border-color: #b16286;
+		background-color: #282828;
+		font-family: monospace;
 		margin-bottom: 10px;
 		font-size: 1rem;
 		padding: 4px;
+
+		caret-color: #b16286;
 	}
 
 	.selected {
-		background-color: red;
+		background-color: #b16286;
 	}
 
 	p {
