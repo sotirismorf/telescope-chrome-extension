f5b3d05bd84b5a5cf339c0c9f33c1865d7b8fd03
Sotiris Morfakidis <sotirismorfakidis@gmail.com>
2022-07-24 00:44:37 +0300

    added basic styling

--- src/components/Options.svelte
+++ src/components/Options.svelte
@@ -5,24 +5,21 @@
 	let tabs: chrome.tabs.Tab[] = [];
 
 	let searchQueryInput: any;
+	let selectedIndex: number = 0;
 
 	onMount(() => {
 		searchQuery = "";
 		onInputChange();
 		searchQueryInput.focus();
 	});
-n
-	let test = "";
-
-	function onSubmit(e) {
-		if (tabs.length === 0) return;
-	}
 
 	function onKeyDown(e: KeyboardEvent) {
 		console.log(e);
 		if (e.code === "Enter") {
-			chrome.tabs.update(tabs[0].id, { active: true });
+			chrome.tabs.update(tabs[selectedIndex].id, { active: true });
 		} else if (e.code === "Tab") {
+			if (e.shiftKey && selectedIndex > 0) selectedIndex -= 1;
+			else if (!e.shiftKey && selectedIndex < 8) selectedIndex += 1;
 		}
 	}
 
@@ -38,23 +35,56 @@ n
 
 <div class="container">
 	<input
-		on:submit={onSubmit}
 		bind:this={searchQueryInput}
 		bind:value={searchQuery}
 		on:input={onInputChange}
 		on:keydown={onKeyDown}
+		placeholder="Search for title"
 		type="text"
 	/>
-	{test}
-	{#each tabs as tab}
-		<p>{tab.title}</p>
-	{/each}
+	<div class="results">
+		{#each tabs as tab, i}
+			<p class="{ i === selectedIndex ? 'selected' : ''}">{tab.title}</p>
+		{/each}
+	</div>
 </div>
 
 <style>
 	.container {
 		width: 400px;
+		display: flex;
+		flex-direction: column;
+		overflow-y: hidden;
+		overflow-x: hidden;
+		font-family: monospace;
+		font-size: 1rem;
+	}
+
+	.results {
+		border: solid;
+		border-color: black;
 		height: 300px;
-		overflow-y: scroll;
+	}
+
+	input {
+		font-family: monospace;
+		border: solid;
+		border-color: black;
+		outline: none;
+		margin-bottom: 10px;
+		font-size: 1rem;
+		padding: 4px;
+	}
+
+	.selected {
+		background-color: red;
+	}
+
+	p {
+		margin: 0;
+		padding: 4px;
+		white-space: nowrap;
+		overflow: hidden;
+		text-overflow: ellipsis;
 	}
 </style>
